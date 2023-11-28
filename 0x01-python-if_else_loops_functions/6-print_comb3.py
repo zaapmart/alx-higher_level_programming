6d22ed767d6b2d8543480c904c8eac5b96a40956
Kufre Akpan <contactkufreakpan@gmail.com>
2023-11-28 12:04:00 -0800

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for firstNumber in range(0 , 10):
+for firstNumber in range(0, 10):
     for secondNumber in range(firstNumber + 1, 10):
         if firstNumber == 8 and secondNumber == 9:
             print("{}{}".format(firstNumber, secondNumber))
