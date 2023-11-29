aa28eb0e3de22852accb494f18cf012a50bdccc1
Kufre Akpan <contactkufreakpan@gmail.com>
2023-11-29 04:05:58 -0800

    print alphabets in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alph in range(ord('z'), ord('a') -1, -2):
-    print("{} {}".format(chr(alph), chr(alph - 33)), end='')
+    print("{i}{}".format(chr(alph), chr(alphi - 33)), end='')
