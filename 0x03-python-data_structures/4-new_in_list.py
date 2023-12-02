ac6fdd8c8d7c0bde2cda958da45ca88ce41bc89b
Kufre Akpan <contactkufreakpan@gmail.com>
2023-12-02 13:49:23 -0800

    updated 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     tmp_list = my_list[:]
     if 0 <= idx < len(my_list):
         tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+        return (tmp_list)
+    return (my_list)
