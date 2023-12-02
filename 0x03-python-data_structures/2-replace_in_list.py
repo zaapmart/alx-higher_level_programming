d5bdfc07976dc6fb47413439f6f77f1894bacd93
Kufre Akpan <contactkufreakpan@gmail.com>
2023-12-02 13:51:35 -0800

    updated 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,13 +2,4 @@
 def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
-    return(my_list)
-
-vi 3-print_reversed_list_integer.py
-
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return
-    for i in reversed(my_list):
-        print('{:d}'.format(i))
+    return (my_list)
