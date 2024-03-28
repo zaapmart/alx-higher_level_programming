61ae9a36d9ae77c754e449c0f93660bc27fdd558
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-27 21:54:38 -0700

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL provided as an argument
-url=$1
-
-# Use curl to send a request to the URL and get the body size of the response
-body_size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-
-# Display the body size in bytes
-echo "$body_size"
-
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
