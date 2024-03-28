eaa5e33759a2430ca55d6b2cbe9575c1114f81b0
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-28 02:20:44 -0700

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
-echo  # Adding an empty echo command to end with a new line
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me && printf "\n"
