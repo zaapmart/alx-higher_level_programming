caee1b8c1c3c35593d1b5976484e5b065727406c
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-29 16:11:53 -0700

    update on github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository “rails”
-by the user “rails”
+Takes your GitHub credentials (username and password) and uses the GitHub API
+to display your id
 """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     """
-    list 10 commits (from the most recent to oldest) of the repository
-    “rails” by the user “rails”
+    Takes your GitHub credentials (username and password) and uses the GitHub API
+    to display your id
     """
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    res_list = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
-                                  get('commit').get('author').get('name')))
-    except:
-        pass
+    username = argv[1]
+    password = argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+    
+    if response.status_code == 200:
+        user_info = response.json()
+        print("Your GitHub id is:", user_info['id'])
+    else:
+        print("Failed to fetch user information. Status code:", response.status_code)
