ec81a4153d267ca01fc6e8b2c52a33f37b490c37
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-29 14:54:02 -0700

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
