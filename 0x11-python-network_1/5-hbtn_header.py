124ad221308a09169ea50a282fb86d9c871fc2ec
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-29 15:45:21 -0700

    updated 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,12 +9,17 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """takes in a URL, sends a request to the URL and displays
-    the value of the X-Request-Id variable found in the header
-    of the response using requests"""
-    r = requests.get(argv[1])
-    try:
-        r_id = r.headers['X-Request-Id']
-        print(r_id)
-    except:
-        pass
+    if len(argv) < 2:
+        print("Usage: python script.py <URL>")
+    else:
+        url = argv[1]
+        try:
+            r = requests.get(url)
+            r.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
+            r_id = r.headers.get('X-Request-Id')
+            if r_id:
+                print(r_id)
+            else:
+                print("X-Request-Id header not found in the response.")
+        except requests.RequestException as e:
+            print("Error occurred while fetching the URL:", e)
