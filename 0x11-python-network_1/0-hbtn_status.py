6a247c72c71c9a83e9cbce1fe37ae925d46f3939
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-29 17:44:08 -0700

    hbtn-status update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     req = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(req) as response:
@@ -12,3 +12,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content.decode('utf-8')))
+
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """
 takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
+http://localhost:5000/api/v1/stats with the letter as a parameter
 """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     """
     takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter
+    http://localhost:5000/api/v1/stats with the letter as a parameter
     """
-    url = 'http://0.0.0.0:5000/search_user'
-    r = requests.get(url)
+    url = 'http://localhost:5000/api/v1/stats'
     if len(argv) == 2:
-        r = requests.post(url, data={'q': argv[1]})
+        letter = argv[1]
     else:
-        r = requests.post(url, data={'q': ""})
+        letter = ""
+        
     try:
-        if r.json() == {}:
-            print("No result")
+        r = requests.post(url, data={'q': letter})
+        response_json = r.json()
+        if response_json:
+            if 'id' in response_json and 'name' in response_json:
+                print("[{}] {}".format(response_json['id'], response_json['name']))
+            else:
+                print("No result")
         else:
-            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
-    except:
+            print("No result")
+    except ValueError:
         print("Not a valid JSON")
+    except requests.RequestException as e:
+        print("Error occurred while making the request:", e)
