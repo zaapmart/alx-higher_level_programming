be22c12aa184316f9011e828679a0be7bd86f22c
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-29 17:39:12 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,11 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     req = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(req) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content.decode('utf-8'))
-
+        print("\t- content: {}".format(content.decode('utf-8')))
