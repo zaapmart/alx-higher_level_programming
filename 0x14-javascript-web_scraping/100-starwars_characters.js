de33dd589c81e6709293e0cf3b6d23964753de63
Kufre Akpan <contactkufreakpan@gmail.com>
2024-04-23 17:30:40 -0700

    fixed url link

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(apiUrl, function (error, response, body) {
   if (!error && response.statusCode === 200) {
